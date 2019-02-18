dfad4d1b8ae3f44c7ec98e2c3e8aab57c072a5e4
Linda <linda.a.zanchi@gmail.com>
2019-02-17 22:00:53 -0500

    one marker shows

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-import {
-	Map,
-	/*Marker, InfoWindow,*/ GoogleApiWrapper
-} from "google-maps-react";
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from "google-maps-react";
 
 const mapStyles = {
 	width: "100%",
@@ -10,6 +7,28 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+	state = {
+		showInfoWindow: false,
+		activeMarker: {},
+		selectedPlace: {}
+	};
+
+	onMarkerClick = (props, marker, event) =>
+		this.setState({
+			selectedPlace: props,
+			activeMarker: marker,
+			showInfoWindow: true
+		});
+
+	onClose = props => {
+		if (this.state.showInfoWindow) {
+			this.setState({
+				showInfoWindow: false,
+				activeMarker: null
+			});
+		}
+	};
+
 	render() {
 		return (
 			<Map
@@ -20,7 +39,18 @@ export class MapContainer extends Component {
 					lat: 36.7421339,
 					lng: -5.1665916
 				}}
-			/>
+			>
+				<Marker onClick={this.props.onMarkerClick} />
+				<InfoWindow
+					marker={this.state.activeMarker}
+					visible={this.state.showInfoWindow}
+					onClose={this.onClose}
+				>
+					<div>
+						<h4>{this.state.selectedPlace.name}</h4>
+					</div>
+				</InfoWindow>
+			</Map>
 		);
 	}
 }
