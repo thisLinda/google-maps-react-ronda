4061c3508606b678f6069fccd42a8ec6fd9f2378
Linda <linda.a.zanchi@gmail.com>
2019-02-27 09:25:34 -0500

    animation working

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -138,11 +138,7 @@ class MapContainer extends Component {
                 key={placeName.name}
                 onClick={this.onMarkerClick}
                 position={placeName.pos}
-                animation={
-                  this.state.activeMarker.name === placeName.name
-                    ? this.props.google.maps.Animation.BOUNCE
-                    : null
-                }
+                animation = {this.state.activeMarker.length === 1 ? this.props.google.maps.Animation.BOUNCE : this.props.google.maps.Animation.DROP}
               />
             );
           })}
